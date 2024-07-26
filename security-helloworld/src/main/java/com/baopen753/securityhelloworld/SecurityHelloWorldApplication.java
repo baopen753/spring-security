03db2b5c83c2d20c52295515e94c92b011171c1d
baopen753 <tranquocbao070503@gmail.com>
2024-07-26 15:51:47 +0700

    #section 1

---
+++ security-helloworld/src/main/java/com/baopen753/securityhelloworld/SecurityHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.baopen753.securityhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHelloWorldApplication.class, args);
+	}
+
+}
---
+++ security-helloworld/src/test/java/com/baopen753/securityhelloworld/SecurityHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baopen753.securityhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
