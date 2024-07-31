e96a42a7b05f63a7e43afea0e020616a32f0f788
baopen753 <tranquocbao070503@gmail.com>
2024-08-01 00:40:41 +0700

    add: security with database config

---
+++ security-with-database-config /src/main/java/com/baopen753/securitywithdatabaseconfig/controller/LoansController.java
@@ -0,0 +1,15 @@
+package com.baopen753.securitywithdatabaseconfig.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+
+    @GetMapping("/myLoans")
+    public ResponseEntity<?> myLoans() {
+        return ResponseEntity.ok("This is my loan's information");
+    }
+
+}
---
+++ security-with-database-config /src/main/resources/sql/data.sql
@@ -0,0 +1,5 @@
+insert into users values ('user','{noop}12345','1');
+insert into users values ('baopen753','{bcrypt}$2a$12$qFZsYy8Zf11qLvUQmsoMmuKf681/k8MoM3a18RqK6Yd3iIbTAgUjm','1');
+
+insert into authorities values ('user', 'read');
+insert into authorities values ('baopen753','admin');
